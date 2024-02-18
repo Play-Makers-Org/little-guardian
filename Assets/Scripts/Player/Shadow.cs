95f05eda50e2efcd80036bec4ec817666a387329
ScarletTx3 <262pirate@gmail.com>
2024-02-18 13:56:29 +0300

    Shadow

---
+++ Assets/Scripts/Player/Shadow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Shadow : MonoBehaviour
+{
+public Vector3 Offset = new Vector3(-0.1f,-0.1f);
+public Material Material;
+
+GameObject  _shadow;
+
+void Start ()
+{
+_shadow=new GameObject ("Shadow");
+_shadow.transform.parent = transform;
+
+_shadow.transform.localPosition = Offset;
+_shadow.transform.localRotation = Quaternion.identity
+
+SpriteRenderer renderer = GetComponent <SpriteRenderer> ();
+SpriteRenderer sr = _shadow.AddComponent <SpriteRenderer> ();
+sr.sprite = renderer.sprite;
+sr.material = Material;
+
+sr.sortingLayerName = renderer.sortingLayerName;
+sr.sortingOrder = renderer.sortingOrder - 1;
+}
+void LateUpdate(){
+_shadow.transform.localPosition = Offset;
+}
+}
